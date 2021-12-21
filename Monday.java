df0862337bed20d25a9e95bdc024c031303de34b
umeshbn <96108616+umeshbn@users.noreply.github.com>
2021-12-21 21:54:43 +0530

    upadate monday.java

--- Monday.java
+++ Monday.java
@@ -2,8 +2,8 @@ class Monday{
   public static void main(String[] args){
 
 	for(int num=1;num<10;num=num+1){
-      		sum=sum+i;
-		System.out.println(sum+1);
+      		
+		System.out.println("uttar kannada");
 	
 
        	
